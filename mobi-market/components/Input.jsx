b10d3d4f191b70a34786b50dc4b4e0d021abd0aa
Akbota <aqbotamaratqyzy@gmail.com>
2023-12-20 23:36:45 +0600

    day 9 prepared profile page

--- mobi-market/components/Input.jsx
+++ mobi-market/components/Input.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 function Input() {
+  const customField={
+    
+  }
   return (
         <div>
           <div style={{width: '100%', height: '100%', paddingTop: 10, paddingBottom: 10, 
           borderBottom: '0.50px #C0C0C0 solid', justifyContent: 'flex-start', alignItems: 'center', 
           gap: 10, display: 'inline-flex', marginBottom:'24px'}}>
-            <div style={{flex: '1 1 0', color: '#C0C0C0', fontSize: 16, fontFamily: 'Inter', 
-            fontWeight: '400', wordWrap: 'break-word'}}>Имя пользователя</div>
+              <label class="custom-field">
+                <input type="email"/>
+                <span class="placeholder">Имя пользователя</span>
+              </label>
           </div>
         </div>
   )
