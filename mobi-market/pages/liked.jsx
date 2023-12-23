13d33025ead611d9c34edbe3d3c6c62b90ecd0ae
Akbota <aqbotamaratqyzy@gmail.com>
2023-12-23 21:13:17 +0600

    day 12 finished preparing main page, worked with pop-up windows

--- mobi-market/pages/liked.jsx
+++ mobi-market/pages/liked.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Grid, Box, Typography } from '@mui/material';
 
-function profile() {
+function liked() {
   return (
     <Grid container spacing={2}>
         <Grid item xs={12} sm={6} md={4.5} lg={4.5}>
             <div style={{width:'100%',height:'100vh'}}>
                 <div style={{display:'flex', flexDirection:'column', alignItems:'flex-start', paddingLeft:'7.7%', paddingRight: '7.7%',paddingTop:'4.4%'}}>
-                    <div style={{paddingBottom:'48px', display:'flex', flexDirection:'row'}}>
+                    <div style={{paddingBottom:'48px', display:'flex', flexDirection:'row', paddingLeft:'8px'}}>
                         <div>
                             <div style={{ width: '60px', height: '60px', borderRadius: '50%', backgroundColor: '#5458EA', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                                 <span>
@@ -21,7 +21,7 @@ function profile() {
                             </div>
                     </div>
 
-                    <div style={{ paddingBottom: '16px', display: 'flex', flexDirection: 'row', alignItems: 'center', width:'100%', opacity:'40%'}}>
+                    <div style={{padding:'8px',display: 'flex', flexDirection: 'row', alignItems: 'center', width:'100%',backgroundColor:'#F7F6F9', justifyContent:'center', borderRadius:'10px'}}>
                         <div style={{ borderRadius: '10%', backgroundColor: '#5458EA', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '30px', height: '30px' }}>
                             <img src='/images/Heart.svg' alt="Heart icon" />
                         </div>
@@ -33,7 +33,7 @@ function profile() {
                             </div>
                     </div>
 
-                    <div style={{ paddingBottom: '48px', display: 'flex', flexDirection: 'row', alignItems: 'center', width:'100%', opacity:'40%'}}>
+                    <div style={{paddingTop:'16px',paddingBottom: '48px',paddingLeft:'8px', display: 'flex', flexDirection: 'row', alignItems: 'center', width:'100%', opacity:'40%'}}>
                         <div style={{ borderRadius: '10%', backgroundColor: '#5458EA', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '30px', height: '30px' }}>
                             <img src='/images/store.svg' alt="Heart icon" />
                         </div>
@@ -45,7 +45,7 @@ function profile() {
                             </div>
                     </div> 
 
-                    <div style={{ paddingBottom: '8px', display: 'flex', flexDirection: 'row', alignItems: 'center', width:'100%'}}>
+                    <div style={{ paddingBottom: '8px', paddingLeft:'8px', display: 'flex', flexDirection: 'row', alignItems: 'center', width:'100%'}}>
                         <div style={{ borderRadius: '10%', backgroundColor: '#5458EA', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '30px', height: '30px' }}>
                             <img src='/images/logout.svg' alt="Heart icon" />
                         </div>
@@ -73,45 +73,33 @@ function profile() {
                     {/* <div style={{marginLeft: 'auto', marginRight: 'auto'}}>Профиль</div> */}
 
                 </div>
-
-                <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-                    <div style={{ width: '60px', height: '60px', borderRadius: '50%', backgroundColor: '#5458EA', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                            <span>
-                            <img src='/images/user.svg'></img>
-                            </span>
-                    </div>
-                    <div style={{paddingTop:'12px', color:'#5458EA', fontFamily:'Inter', fontSize:'16px', paddingBottom:'32px'}}>Выбрать фотографию</div>
-                </div>
-
-                <div style={{display:'flex', flexDirection:'column', backgroundColor:'#FFFFFF', borderRadius:'12px', fontSize:'16px', paddingLeft:'16px', color:'#C0C0C0'}}>
-                    <div style={{display:'flex', alignContent:'center', borderBottom: '1px solid #F7F6F9', paddingTop:'8px', paddingBottom:'8px'}}>Имя</div>
-                    <div style={{borderBottom: '1px solid #F7F6F9', paddingTop:'8px', paddingBottom:'8px'}}>Фамилия</div>
-                    <div style={{borderBottom: '1px solid #F7F6F9', paddingTop:'8px', paddingBottom:'8px'}}>Akbota Kurmangazhyyeva</div>
-                    <div style={{paddingTop:'8px', paddingBottom:'8px'}}>гггг-мм-дд</div>
-                </div>
-
-                <div style={{paddingTop:'12px'}}>
-                <div style={{display:'flex', flexDirection:'column', backgroundColor:'#FFFFFF', borderRadius:'12px', fontSize:'16px', paddingLeft:'16px',paddingRight:'16px', color:'#C0C0C0', fontFamily:'Inter'}}>
-                    <div style={{borderBottom: '1px solid #F7F6F9', paddingTop:'8px', paddingBottom:'8px', color:'#5458EA', display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
-                        <div>Добавить номер</div>
-                        <div style={{color:'#C0C0C0'}}>0(000)00000</div>
-                        </div>
-                    <div style={{borderBottom: '1px solid #F7F6F9', paddingTop:'8px', paddingBottom:'8px'}}>nikitina.alesya@gmail.</div>
-                </div>
-                </div>
-
-                <div style={{display:'flex',justifyContent: 'center', alignItems: 'center', paddingTop:'44px'}}>
-                <div style={{paddingTop: 10,paddingBottom: 10,paddingLeft: 36,paddingRight:36, background: '#5458EA', borderRadius: 80, gap: 10, fontSize:'16px', fontFamily:'Inter'}}>
-                    <div style={{color: 'white', fontSize: 16, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>
-                        Закончить регистрацию</div>
-                </div>
-                </div>
-
+            <div>
+            <Grid container spacing={2}>
+      {Array.from({ length: 2 }, (_, row) => (
+        <Grid container item key={row} spacing={2} style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+          {Array.from({ length: 4}, (_, col) => (
+            <Grid item key={col} xs={3} lg={3} sm={3} md={3}>
+              {/* Your content for each grid item */}
+              <div style={{ backgroundColor:'#FFFFFF', borderRadius:'10px'}}>
+                  <div style={{display:'flex', flexDirection:'column', padding:'6px', gap:4}}>
+                    <div><img src="/images/car.png" height='auto' style={{maxWidth:"100%"}}/></div>
+                    <div style={{fontWeight:'600'}}>BMW M4 Coupe: A Two-Door</div>
+                    <div style={{color:'#5D5FEF'}}>23000 $</div>
+                    <div style={{display:'flex', flexDirection:'row', gap:4, alignItems:'center'}}><img src='/images/like.svg'></img><div style={{fontSize:'14px'}}>100</div></div>
+                  </div>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      ))}
+    </Grid>
             </div>
+                
+        </div>
         </div>
         </Grid>
     </Grid>
   )
 }
 
-export default profile
+export default liked
--- mobi-market/pages/main.jsx
+++ mobi-market/pages/main.jsx
@@ -1,32 +1,69 @@
 import React from 'react'
 import { Grid, Box, Typography } from '@mui/material';
+import Image from 'next/image'
 
 function main() {
   return (
-    <div style={{paddingLeft:'44px', paddingRight:'44px'}}>
+    <div style={{paddingLeft:'100px', paddingRight:'100px', paddingTop:'20px', backgroundColor:'#F0F0F0'}}>
         <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between', paddingBottom:'40px'}}>
-            <div>
-                Logo
+            <div style={{display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                <div>
+                    <img src="/images/shopping-cart.svg" width="44px" height="44px"></img>
+                </div>
+                <div style={{fontFamily:'Nunito', fontWeight:'800px', fontSize:'20px', paddingLeft:'8px'}}>
+                  MOBI MARKET
+                </div>
             </div>
-            <div style={{display:'flex', flexDirection:'row'}}>
-                <div>Подать объявление</div>
-                <div>Аккаунт</div>
+            <div style={{display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                <div>
+                <div style={{ padding: 10, background: '#5458EA', borderRadius: 80, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
+                  <div style={{color: 'white', fontSize: 16, fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word'}}>Подать объявление</div>
+                </div>
+                </div>
+                <div style={{paddingLeft:'16px'}}>
+                <div style={{ display:'flex', flexDirection:'row'}}>
+                            <div style={{display:'flex', flexDirection:'column', marginRight:'12px', justifyContent:'center',fontFamily:'Inter', alignItems:'flex-end'}}>
+                                <div style={{fontWeight:'600'}}>Akbota Kurmangazhyyeva</div>
+                                <div>youngccamel</div>
+                            </div>
+                        <div>
+                            <div style={{ width: '60px', height: '60px', borderRadius: '50%', backgroundColor: '#5458EA', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <span>
+                                    <img src='/images/user.svg'></img>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <Grid container spacing={2}>
       {Array.from({ length: 4 }, (_, row) => (
-        <Grid container item key={row} spacing={2}>
+        <Grid container item key={row} spacing={2} style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
           {Array.from({ length: 8 }, (_, col) => (
-            <Grid item key={col} xs={1.5}>
+            <Grid item key={col} xs={6} lg={1.5} sm={4} md={3}>
               {/* Your content for each grid item */}
-              <div style={{ height: '17.9vh', border: '1px solid #ddd' }}>
-                Product
+              <div style={{ backgroundColor:'#FFFFFF', borderRadius:'10px'}}>
+                  <div style={{display:'flex', flexDirection:'column', padding:'6px', gap:4}}>
+                    <div><img src="/images/car.png" height='auto' style={{maxWidth:"100%"}}/></div>
+                    <div style={{fontWeight:'600'}}>BMW M4 Coupe: A Two-Door</div>
+                    <div style={{color:'#5D5FEF'}}>23000 $</div>
+                    <div style={{display:'flex', flexDirection:'row', gap:4, alignItems:'center'}}><img src='/images/like.svg'></img><div style={{fontSize:'14px'}}>100</div></div>
+                  </div>
               </div>
             </Grid>
           ))}
         </Grid>
       ))}
     </Grid>
+        <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'center', gap:4, paddingTop:'40px', paddingBottom:'40px'}}>
+            <div style={{height:'31px', width:'31px', borderRadius:'10px', border:'1px solid #5458EA', display:'flex', alignItems:'center', justifyContent:'center'}}><img src="/images/arrow-left-blue.svg"></img></div>
+            <div style={{height:'32px', width:'32px', borderRadius:'10px', backgroundColor:'#C0C0C0',display:'flex', alignItems:'center', justifyContent:'center', fontSize:'14px'}}>1</div>
+            <div style={{height:'32px', width:'32px', borderRadius:'10px', backgroundColor:'#5458EA',display:'flex', alignItems:'center', justifyContent:'center', fontSize:'14px', color:'#FFFFFF'}}>2</div>
+            <div style={{height:'32px', width:'32px', borderRadius:'10px', backgroundColor:'#C0C0C0',display:'flex', alignItems:'center', justifyContent:'center', fontSize:'14px'}}>3</div>
+            <div style={{height:'31px', width:'31px', borderRadius:'10px', border:'1px solid #5458EA', display:'flex', alignItems:'center', justifyContent:'center'}}><img src="/images/arrow-right-blue.svg"></img></div>
+        </div>
+
     </div>
   )
 }
